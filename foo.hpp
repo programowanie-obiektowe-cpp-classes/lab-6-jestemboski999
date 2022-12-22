ccc7a454e9eacd64cbca6c71297dadf88ff3d597
jestemboski999 <jestemboski999@gmail.com>
2022-12-22 10:42:37 +0100

    v1

--- foo.hpp
+++ foo.hpp
@@ -9,8 +9,9 @@
 std::vector< char > foo(std::list< Human >& people)
 {
     std::vector< char > retval(people.size());
-
-    // Twoja implementacja tutaj
-
+    std::vector< char > retval(people.size());
+	std::for_each(people.begin(), people.end(), [](Human &n) { n.birthday(); });
+	std::transform(people.begin(), people.end(), retval.begin(), [](const Human &n) { return n.isMonster() ? 'n' : 'y'; });
+	std::reverse(retval.begin(), retval.end());
     return retval;
 }
